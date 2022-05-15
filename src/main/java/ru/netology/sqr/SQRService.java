ac513afa4fd9a3e6dedb9270dc9604ffc88f319a
Tatiana Lyapina <antipts@gmail.com>
2022-05-15 16:53:56 +0300

    Update the conditions for limits

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,25 +2,18 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-  public int squareCalculation (int limit1, int limit2) {
+    public int squareCalculation(int limit1, int limit2) {
 
         int count = 0;
 
         for (int i = 10; i <= 99; i++) {
             int x = i * i;
 
-            //  if (x >= limit1) {
-            //     if (x <= limit1) {
-            //     count++;
-            //     }
-            //  }
+            if ((x >= limit1) && (x <= limit2)) {
+                count++;
 
-            for (int c = limit1; c <= limit2; c++) {
-
-                if (c == x) {
-                    count++;
-                }
             }
+
         }
         return count;
     }
